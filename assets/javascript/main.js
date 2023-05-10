136be2f821ac5ae6786b341b5522f258efbe3e58
nachoescribano <dialogasl@gmail.com>
2023-05-10 18:15:57 +0200

    más mejoras botón menú móvil

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -40,79 +40,21 @@ try {
   window.removeEventListener("testPassive", null, opts);
 } catch (e) {}
 
+const navMenuList = document.querySelector(".js-navMenuList");
+const buttonMenuMobile = document.querySelector(".js-buttonMenuMobile");
 const elementTarget = document.getElementById("section-2");
 let myBar;
 
+/*navMenuList.classList.remove("nav-menu__list--deactivate-animation");*/
 
-const chartData = {
-  labels: [],
-  datasets: [
-    {
-      label: [],
-      data: [],
-      backgroundColor: "#003A6A",
-      borderWidth: 0,
-    },
-  ],
-};
-
-const barOptions = {
-  events: false,
-  showTooltips: false,
-  legend: {
-    position: "bottom",
-  },
-  scales: {
-    yAxes: [
-      {
-        gridLines: {
-          drawBorder: false,
-        },
-        ticks: {
-          beginAtZero: true,
-          padding: 10,
-        },
-      },
-    ],
-    xAxes: [
-      {
-        gridLines: {
-          drawOnChartArea: false,
-        },
-      },
-    ],
-  },
-  animation: {
-    duration: 500,
-    easing: "easeInSine",
-    onComplete: function () {
-      let fontSize = 13;
-      if (document.body.clientWidth > 1024) {
-        fontSize = 18;
-      }
-      let ctx = this.chart.ctx;
-      ctx.font = Chart.helpers.fontString(
-        fontSize,
-        "bold",
-        Chart.defaults.global.defaultFontFamily
-      );
-      ctx.textAlign = "center";
-      ctx.textBaseline = "bottom";
-      for (let i = 0; i < this.data.datasets.length; i++) {
-        const dataset = this.data.datasets[i];
-        for (let e = 0; e < dataset.data.length; e++) {
-          const model =
-            dataset._meta[Object.keys(dataset._meta)[0]].data[e]._model;
-          ctx.fillStyle = "#003A6A";
-          ctx.fillText(
-            Intl.NumberFormat("de-DE").format(dataset.data[e]),
-            model.x,
-            model.y - 5
-          );
-        }
-      }
-    },
-  },
+const menuBtnFn = () => {
+  navMenuList.classList.toggle("nav-menu__list--show");
+  navMenuList.classList.toggle("nav-menu__list--hide");
+  if (!navMenu.classList.includes("nav-menu--white")) {
+    header.classList.toggle("header--force-bg-white");
+    navMenu.classList.toggle("nav-menu--force-white");
+    separatorLogo.classList.toggle("separtator-logo--force");
+  }
 };
 
 function goToAnchor(event) {
